3006af486d417558df7fe183b264f2f444f9fc52
Codru <135108574+Codru1@users.noreply.github.com>
2026-09-17 20:57:05 +0300

    Delete OmniChall-CTFtime-Organizer/scripts/wordpress-entrypoint.sh

--- OmniChall-CTFtime-Organizer/scripts/wordpress-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-printf '%s\n' "${CTF_FLAG:-CTF{customer_area_path_escape}}" > /opt/flag.txt
-chown root:www-data /opt/flag.txt
-chmod 0440 /opt/flag.txt
-
-exec docker-entrypoint.sh "$@"
